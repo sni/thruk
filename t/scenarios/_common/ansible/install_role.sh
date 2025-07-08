ceac158c779921d7bf4111a3f481db1e7de8c4b5
Sven Nierlein <sven@consol.de>
2025-07-08 14:35:54 +0200

    tests: improve ansible helper

--- t/scenarios/_common/ansible/install_role.sh
+++ t/scenarios/_common/ansible/install_role.sh
@@ -3,6 +3,13 @@
 ROLE=$1
 shift
 
+if [ "$ROLE" = "" ]; then
+  echo "Usage: $0 <role>"
+  echo "available roles:"
+  ls -1 $ANSIBLE_ROLES_PATH
+  exit 1
+fi
+
 export ANSIBLE_RETRY_FILES_ENABLED="False"
 ansible-playbook -f1 -i localhost, -c local -e SITENAME=demo "$@" /dev/stdin <<END
 ---
