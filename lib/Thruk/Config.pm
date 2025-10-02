93b2d0612481bf37a911dea763583dc163da53b7
Sven Nierlein <Sven.Nierlein@consol.de>
2025-10-02 09:21:16 +0200

    releae v3.24

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -26,7 +26,7 @@ Generic Access to Thruks Config
 
 ######################################
 
-our $VERSION = '3.22.2';
+our $VERSION = '3.24';
 our $GIT_HASH = ""; # set by dailydist make target
 
 our $config;
@@ -35,7 +35,7 @@ our $project_root = home() || confess('could not determine project_root from inc
 my $base_defaults = {
     'name'                                  => 'Thruk',
     'fileversion'                           => $VERSION,
-    'released'                              => 'May 28, 2025',
+    'released'                              => 'October 02, 2025',
     'compression_format'                    => 'gzip',
     'ENCODING'                              => 'utf-8',
     'image_path'                            => $project_root.'/root/thruk/images',
--- support/thruk.spec
+++ support/thruk.spec
@@ -9,8 +9,8 @@
 %endif
 
 Name:          thruk
-Version:       3.22.2
-Release:       13974.1
+Version:       3.24
+Release:       14133.1
 License:       GPL-2.0-or-later
 Packager:      Sven Nierlein <sven.nierlein@consol.de>
 Vendor:        Labs Consol
