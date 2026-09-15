f46a9b6d0a226b4581d54c4f22fb380a88cf7b6f
Sven Nierlein <Sven.Nierlein@consol.de>
2026-09-15 13:21:04 +0200

    add option require_comments_for_disable_cmds to skip errors when at least one command succeded

--- lib/Thruk/Controller/Rest/V1/cmd.pm
+++ lib/Thruk/Controller/Rest/V1/cmd.pm
@@ -326,6 +326,12 @@ sub _rest_get_external_command {
                     }
                 }
             }
+
+            # allow overriding exit code for partial errors, only some backends down
+            if($c->config->{'rest_skip_command_partial_errors'}) {
+                $data->{'code'}    = 200;
+                $data->{'warning'} = delete $data->{'error'};
+            }
         }
 
         return $data;
